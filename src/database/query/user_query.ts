853f948d73310ce1f4771d5067c52dc55ce48b8d
hadi <hadi@depository.id>
2024-01-23 23:52:02 +0700

    create user

--- src/database/query/user_query.ts
+++ src/database/query/user_query.ts
@@ -1,3 +1,5 @@
+import { UserModel } from '../../model/user_model';
+import { Moment } from '../../util/moment';
 import db from '../database'
 import { Query } from './query';
 
@@ -5,10 +7,22 @@ export class UserQuery implements Query {
     index(): string {
         throw new Error("Method not implemented.");
     }
-    create(): string {
-        throw new Error("Method not implemented.");
+    create(user: UserModel): string {
+        return `
+        INSERT INTO 
+            users 
+            (name, email, password, role, status_id, created_at, updated_at)
+        VALUES
+            (${db.escape(user.getName())}, 
+            ${db.escape(user.getEmail())}, 
+            ${db.escape(user.getPassword())},
+            ${db.escape(user.getRole())},
+            1,
+            '${Moment.getCurrent()}',
+            '${Moment.getCurrent()}');
+        `
     }
-    
+
     show(): string {
         throw new Error("Method not implemented.");
     }
@@ -16,3 +30,13 @@ export class UserQuery implements Query {
         throw new Error("Method not implemented.");
     }
 }
+
+// db.query(`
+//         INSERT INTO users
+//         (id, name, email, password, status_id, role)
+//         VALUES 
+//         (1, 'ROOT', 'root@mail.com', '$2a$10$3zoB8RuTA1JfVRCPgESWQuEGNdxqCqzX9K0KNbJHsF0iN04fVQp/y', 3, 'root');
+//         `,(error, result)=>{
+//             if (error) return console.log(error)
+//             console.log(`✅ seeder table ${this.table_name} berhasil`)
+//         } )
--- src/midleware/jwt_middleware.ts
+++ src/midleware/jwt_middleware.ts
@@ -8,8 +8,9 @@ export function jwtCheck(req:Request, res:Response, next:Function){
 
     const api_key = req.headers["x-api-key"]
     if(api_key != config["api-key"]) FailedResponse.apiKeyFailed(res,'')
-    const jwt_token = req.headers["authorization"]
+    const jwt_token = req.headers["authorization"]?.split(" ")[1]
     jwt.verify(jwt_token as string, config.jwt.key, (err: any, user: any) => {
         if (err) return FailedResponse.jwtFailed(res, '') 
+        next()
     });
 }
