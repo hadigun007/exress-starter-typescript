d220d82fd03504dbff56d947a6c8de7870d54894
hadi <hadi@depository.id>
2024-02-01 20:02:33 +0700

    dd test index user

--- src/__tests__/routes/user.routes.test.ts
+++ src/__tests__/routes/user.routes.test.ts
@@ -4,6 +4,7 @@ import { UserModel } from "../../model/user_model";
 import private_route from "../../routes/private_routes";
 import config from '../../../config.json'
 import { JwtUtil } from "../../util/jwt_util";
+import {faker} from '@faker-js/faker'
 
 
 
@@ -13,12 +14,22 @@ const token = JwtUtil.getJwt()
 
 describe("User", () => {
 
+    it("Index User", async () => {
+
+        const index = await request(private_route)
+        .get("/users")
+        .set({ 'x-api-key': config["api-key"], Accept: 'application/json', 'Authorization':'Bearer '+token})
+
+        expect(index.status).toEqual(200)
+        expect(index.body.message).toEqual('success')
+
+    })
+   
     it("Create User", async () => {
-        user.setName('hadi4')
-        user.setEmail('hadi4@mail.com')
+        user.setName(faker.person.fullName())
+        user.setEmail(faker.internet.email())
         user.setPassword('adminsku')
         user.setRole('admin')
-
 
         const create = await request(private_route)
         .post("/user/create")
@@ -29,4 +40,5 @@ describe("User", () => {
         expect(create.body.message).toEqual('success')
 
     })
+
 })
